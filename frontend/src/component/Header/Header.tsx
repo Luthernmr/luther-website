aa70618820bce44607f75f04e8a6b217cf912bac
luther <luther.nemor@atos.net>
2024-09-17 16:03:52 +0200

    fix footer malt img

--- frontend/src/component/Header/Header.tsx
+++ frontend/src/component/Header/Header.tsx
@@ -4,20 +4,22 @@ import { FaGithub, FaLinkedin } from "react-icons/fa";
 import SocialButton from "../SocialButton";
 import MaltButton from "../MaltButton";
 interface HeaderProps {
-  onOpen? : () => void
+  onOpen?: () => void
 }
 const Header: React.FC<HeaderProps> = ({ onOpen }) => {
+
+
   return (
     <Flex
       h={"100%"}
       color={"white"}
       flexDirection={"row"}
-      minW={{ base:"90%", md:"80%"}}
+      minW={{ base: "90%", md: "80%" }}
       justifyContent={"space-between"}
       alignItems={"center"}
       opacity={0.9}
     >
-      <Text align={"center"} fontSize={{base : "xl", md:"2xl"}} fontWeight={"light"}>
+      <Text align={"center"} fontSize={{ base: "xl", md: "2xl" }} fontWeight={"light"}>
         LUTHER NEMOR
       </Text>
       <HStack
@@ -27,12 +29,12 @@ const Header: React.FC<HeaderProps> = ({ onOpen }) => {
         backdropFilter="auto"
         backdropBlur="1px"
         padding={2}
-        paddingLeft={{base:"3", md:"10"}}
-        paddingRight={{base:"3", md:"10"}}
+        paddingLeft={{ base: "3", md: "10" }}
+        paddingRight={{ base: "3", md: "10" }}
         border={"1px solid"}
         borderColor={"whiteAlpha.100"}
-        spacing={{base:"2", md:"8"}}
-        
+        spacing={{ base: "2", md: "8" }}
+
       >
         <Link display={{ base: "none", md: 'flex' }} href={"#services"}>ACCUEIL</Link>
         <Link display={{ base: "none", md: 'flex' }} onClick={onOpen}>RÉSERVER UN CRÉNEAU</Link>
@@ -42,7 +44,6 @@ const Header: React.FC<HeaderProps> = ({ onOpen }) => {
           href={"https://www.malt.fr/profile/luthernemor"}
         >
           <Image src="/logo-malt.png" />
-          
         </MaltButton>
         <SocialButton
           label={"Linkedin"}
