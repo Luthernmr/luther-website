ff4997f8fe47e10df2b9b80fc9932ad57e73fe0c
Luthernmr <nemorluther@gmail.com>
2024-10-06 19:21:47 +0200

    redeploy without contnet

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,21 @@
 
 import Landing from "./component/Landing/Landing";
-import { BrowserRouter, Routes, Route, Navigate  } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import DocsLayout from "./component/Layout/DocsLayout"
-import Content from "./component/Landing/Content";
 import MarkdownViewer from "./component/Layout/MarkdownViewer"
 
 function App() {
- 
+
 
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Landing/>} />
-          <Route path="/docs" element={<DocsLayout/>} >
+          <Route path="/" element={<Landing />} />
+          <Route path="/docs" element={<DocsLayout />} >
             <Route path="/docs" element={<Navigate to="/docs/overview/context" replace />} />
 
-           <Route path="/docs/:category/:docName" element={<MarkdownViewer/>} />
+            <Route path="/docs/:category/:docName" element={<MarkdownViewer />} />
 
           </Route>
         </Routes>
